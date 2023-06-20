40aa121d5fb0a7d5c72968116a54287e706fe2d2
FrozenArcher <yhy242989105@hotmail.com>
2023-06-21 00:31:05 +0800

    add dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,11 @@
+-- plugins.dashboard: startup dashboard
+return {
+    "glepnir/dashboard-nvim",
+    event = "VimEnter",
+    opts = {
+        theme = "hyper",
+    },
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+}
