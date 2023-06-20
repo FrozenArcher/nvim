2f9ab3bb3bcd3db898557d2d7117c89e4fba5403
FrozenArcher <yhy242989105@hotmail.com>
2023-06-21 02:12:12 +0800

    fix telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,16 +4,12 @@ return {
 	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"telescope-fzf-native.nvim",
+		"nvim-telescope/telescope-fzf-native.nvim",
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
 		require("telescope").setup()
 		local builtin = require("telescope.builtin")
-		-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-		-- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-		-- vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-		-- vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 		local nmap = require("utils.keys").nmap
 		nmap("<leader>ff", builtin.find_files, "Find file")
 		nmap("<leader>fg", builtin.live_grep, "Live grep")
