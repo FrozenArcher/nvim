740fe249b6a7cf62b0451afbc2a5479943b9132f
FrozenArcher <yhy242989105@hotmail.com>
2023-06-20 22:52:13 +0800

    change formatter to lsp-format

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,129 +1,124 @@
 -- plugins.lsp: lspconfig, mason
 return {
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			"williamboman/mason.nvim",
-			"williamboman/mason-lspconfig.nvim",
-			--"j-hui/fidget.nvim",
-			{ "folke/neodev.nvim", opts = {} },
-			"RRethy/vim-illuminate",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			-- Set up Mason before anything else
-			require("mason").setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"pyright",
-					"clangd",
-					"rust_analyzer",
-					"gopls",
-				},
-				automatic_installation = true,
-			})
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            --"j-hui/fidget.nvim",
+            { "folke/neodev.nvim", opts = {} },
+            "RRethy/vim-illuminate",
+            "hrsh7th/cmp-nvim-lsp",
+            "lukas-reineke/lsp-format.nvim",
+        },
+        config = function()
+            -- Set up Mason before anything else
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗",
+                    },
+                },
+            })
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "pyright",
+                    "clangd",
+                    "rust_analyzer",
+                    "gopls",
+                },
+                automatic_installation = true,
+            })
 
-			-- vim-illuminate
-			require("illuminate").configure({
-				providers = {
-					"lsp",
-					"treesitter",
-					"regex",
-				},
-				delay = 100,
-			})
-			vim.cmd("hi def IlluminatedWordText gui=underline")
-			vim.cmd("hi def IlluminatedWordRead gui=underline")
-			vim.cmd("hi def IlluminatedWordWrite gui=underline")
+            -- vim-illuminate
+            require("illuminate").configure({
+                providers = {
+                    "lsp",
+                    "treesitter",
+                    "regex",
+                },
+                delay = 100,
+            })
+            vim.cmd("hi def IlluminatedWordText gui=underline")
+            vim.cmd("hi def IlluminatedWordRead gui=underline")
+            vim.cmd("hi def IlluminatedWordWrite gui=underline")
 
-			-- Quick access via keymap
-			require("utils.keys").nmap("<leader>m", "<cmd>Mason<cr>")
+            -- Quick access via keymap
+            require("utils.keys").nmap("<leader>m", "<cmd>Mason<cr>")
 
-			-- Turn on LSP status information
-			--require("fidget").setup()
+            -- Turn on LSP status information
+            --require("fidget").setup()
 
-			-- Set up cool signs for diagnostics
-			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-			end
+            -- Set up lsp-format
+            require("lsp-format").setup({})
 
-			-- Diagnostic config
-			local config = {
-				virtual_text = false,
-				signs = {
-					active = signs,
-				},
-				update_in_insert = true,
-				underline = true,
-				severity_sort = true,
-				float = {
-					focusable = true,
-					style = "minimal",
-					border = "rounded",
-					source = "always",
-					header = "",
-					prefix = "",
-				},
-			}
-			vim.diagnostic.config(config)
+            -- Set up cool signs for diagnostics
+            local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+            end
 
-			-- This function gets run when an LSP connects to a particular buffer.
-			local on_attach = function(client, bufnr)
-				local lsp_map = require("helpers.keys").lsp_map
+            -- Diagnostic config
+            local config = {
+                virtual_text = false,
+                signs = {
+                    active = signs,
+                },
+                update_in_insert = true,
+                underline = true,
+                severity_sort = true,
+                float = {
+                    focusable = true,
+                    style = "minimal",
+                    border = "rounded",
+                    source = "always",
+                    header = "",
+                    prefix = "",
+                },
+            }
+            vim.diagnostic.config(config)
 
-				-- Create a command `:Format` local to the LSP buffer
-				vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-					vim.lsp.buf.format()
-				end, { desc = "Format current buffer with LSP" })
+            -- This function gets run when an LSP connects to a particular buffer.
+            local on_attach = function(client, bufnr)
+                require("lsp-format").on_attach(client)
+                require("illuminate").on_attach(client)
+            end
 
-				lsp_map("<leader>ff", "<cmd>Format<cr>", bufnr, "Format")
+            -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-				-- Attach and configure vim-illuminate
-				require("illuminate").on_attach(client)
-			end
+            -- Lua
+            require("lspconfig")["lua_ls"].setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            library = {
+                                [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                [vim.fn.stdpath("config") .. "/lua"] = true,
+                            },
+                        },
+                    },
+                },
+            })
 
-			-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-			-- Lua
-			require("lspconfig")["lua_ls"].setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-				settings = {
-					Lua = {
-						completion = {
-							callSnippet = "Replace",
-						},
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							library = {
-								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-								[vim.fn.stdpath("config") .. "/lua"] = true,
-							},
-						},
-					},
-				},
-			})
-
-			-- Python
-			require("lspconfig")["pyright"].setup({
-				on_attach = on_attach,
-				capabilities = capabilities,
-			})
-		end,
-	},
+            -- Python
+            require("lspconfig")["pyright"].setup({
+                on_attach = on_attach,
+                capabilities = capabilities,
+            })
+        end,
+    },
 }
